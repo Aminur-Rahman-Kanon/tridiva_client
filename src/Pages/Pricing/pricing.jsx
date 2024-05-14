7c105a90408b07b12cbca7d07a146e8ae6af9e3e
Aminur-Rahman-Kanon <kanon754@gmail.com>
2024-05-14 21:29:10 +0100

    product page 5

---
+++ src/Pages/Pricing/pricing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './pricing.module.css';
+
+const Pricing = () => {
+
+    return (
+        <div className={styles.pricingWrapper}>
+
+        </div>
+    )
+}
+
+export default Pricing;
